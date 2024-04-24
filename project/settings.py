5670f2997025242c0aeb78ed3b3b4351f3efc47c
Gooli <leandro.gooli@gmail.com>
2024-04-24 21:21:06 +0000

    Update: settings

--- project/settings.py
+++ project/settings.py
@@ -119,9 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = (
-    BASE_DIR / 'base_static', #necessário para o projeto reconhecer o base_static
-)
 
 STATIC_ROOT = BASE_DIR / 'static'
 
